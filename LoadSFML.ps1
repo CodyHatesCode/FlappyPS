eb7851dc2308f44cdfcc8885e0f1e8983ad8e871
Cody <codywritescode@gmail.com>
2016-10-16 19:28:18 -0500

    Cleanup

--- LoadSFML.ps1
+++ LoadSFML.ps1
@@ -1,6 +1,6 @@
 # Assemblies --------------------------------------------------------------------------
-Add-Type -Path "sfmlnet-system-2.dll"
-Add-Type -Path "sfmlnet-graphics-2.dll"
-Add-Type -Path "sfmlnet-window-2.dll"
+Add-Type -Path ".\DLL\sfmlnet-system-2.dll"
+Add-Type -Path ".\DLL\sfmlnet-graphics-2.dll"
+Add-Type -Path ".\DLL\sfmlnet-window-2.dll"
 
 Write-Host SFML.NET loaded.
